3b0bf45f55ae07766ef77c6df18cc416da9e6553
leianhua <leianhua1@flkj.ai>
2021-12-15 15:05:05 +0800

    feat:first test img_obtain

---
+++ face_recognition/img_obtain.py
@@ -0,0 +1,23 @@
+import cv2 as cv
+
+# 调用摄像头
+cap = cv.VideoCapture(0)
+
+num = 1
+
+while(cap.isOpened()):
+    ret_flag, Vshow = cap.read()
+    cv.imshow('Capture_Test', Vshow)
+    k = cv.waitKey(1) & 0xFF
+    if k == ord('s'):   # 保存
+        cv.imwrite('img/'+str(num)+'.myface'+'.jpg', Vshow)
+        print('success to save'+str(num)+'.jpg')
+        print('-----------------------')
+        num += 1
+    elif k == ord(' '):  # 退出
+        break
+
+# 释放内存
+cv.destroyAllWindows()
+# 释放摄像头
+cap.release()
