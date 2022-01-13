c5f7ee2f937f55dedfd02966ef8308703d88101e
leianhua <leianhua1@flkj.ai>
2022-01-13 10:37:22 +0800

    feat:增加人脸识别匹配度检验

--- face_recognition/face_research.py
+++ face_recognition/face_research.py
@@ -1,20 +1,25 @@
 from tencentcloud.common import credential
 from tencentcloud.common.exception.tencent_cloud_sdk_exception import TencentCloudSDKException
 from tencentcloud.iai.v20200303 import iai_client, models
-from base.conf_obtain import sys_config
 import base64
 import json
 import time
 
 
 def face_research(img_dir):
+    """
+    人脸搜索
+    img_dir: 需要搜索的图片
+    return: PersonId, PersonName
+    """
     start = time.time()
+    # img_dir = 'C:/Users/bjfqdclf/OneDrive/专属文件夹/学习/毕业设计\program/opencv_demo/img/1.name.jpg'
     with open(img_dir, 'rb') as f:
         base64_data = base64.b64encode(f.read())
         base64_code = base64_data.decode()
     try:
         # 实例化认证对象
-        cred = credential.Credential(sys_config.SecretId, sys_config.SecretKey)
+        cred = credential.Credential("AKIDylIYXFgqiKeiV1GFwMVMxGI2WjfKjriI", "KOAfspVt0rRkmX2FBqEK9hzzjBMIejX3")
         client = iai_client.IaiClient(cred, "ap-shanghai")
 
         # 实例化请求对象
@@ -30,16 +35,20 @@ def face_research(img_dir):
         resp = client.SearchFacesReturnsByGroup(req)
         resp_json = resp.to_json_string()
         resp_dic = json.loads(resp_json)
+
         ResultsReturnsByGroup = resp_dic['ResultsReturnsByGroup'][0]
         GroupCandidates = ResultsReturnsByGroup['GroupCandidates'][0]
         Candidates = GroupCandidates['Candidates'][0]
         PersonId = Candidates['PersonId']
         PersonName = Candidates['PersonName']
+        Score = Candidates['Score']  # 匹配度(官方SDK文档建议80)
         print('id>>', PersonId)
         print('name>>', PersonName)
-        print(time.time() - start)
+        print('本次搜索用时>>', time.time() - start)
+        if Score < 80:
+            print('人脸匹配度低')
+            return False
         return PersonId, PersonName
-
 
     except TencentCloudSDKException as err:
         print(err)
