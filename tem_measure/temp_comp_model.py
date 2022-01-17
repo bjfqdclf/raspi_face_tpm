bf71f4d0968009797aefea0dd67e8a4d30e29e5b
leianhua <leianhua1@flkj.ai>
2022-01-17 10:51:07 +0800

    feat:重构温度补偿接口

--- tem_measure/temp_comp_model.py
+++ tem_measure/temp_comp_model.py
@@ -2,41 +2,38 @@ import math
 
 
 class TempComp:
-    def __init__(self, wrist_temp=None, forehead_temp=None, outside_temp=None):
-        self.wrist_temp = wrist_temp
-        self.forehead_temp = forehead_temp
+    def __init__(self, obj_temp, outside_temp=None):
+        self.obj_temp = obj_temp
         self.outside_temp = outside_temp
-        if wrist_temp and outside_temp:
-            self.wrist_to_forehead_temp()
-        else:
-            self.forehead_to_wrist_temp()
 
     def wrist_to_forehead_temp(self):
         """腕温--->额温  补偿"""
-        wrist_temp = self.wrist_temp
+        wrist_temp = self.obj_temp
         outside_temp = self.outside_temp
         # self._distance_comp(wrist_temp, distance=3)  # 可以不考虑距离补偿
         wrist_temp_2 = math.pow(wrist_temp, 2)
         wrist_temp_3 = math.pow(wrist_temp, 3)
-        self.forehead_temp = 0.02411 * wrist_temp_3 - 2.288 * wrist_temp_2 - 0.004356 * wrist_temp_2 * outside_temp + 0.2851 * wrist_temp * outside_temp - 4.702 * outside_temp + 72.46 * wrist_temp - 728.9
+        forehead_temp = 0.02411 * wrist_temp_3 - 2.288 * wrist_temp_2 - 0.004356 * wrist_temp_2 * outside_temp + 0.2851 * wrist_temp * outside_temp - 4.702 * outside_temp + 72.46 * wrist_temp - 728.9
+        return forehead_temp
 
     def forehead_to_wrist_temp(self):
         """
+        弃用!!!
+
         额温--->腕温  补偿
         由腕温--->额温补偿公式推导
         """
-        forehead_temp = self.forehead_temp
-        outside_temp = self.outside_temp
+        # forehead_temp = self.forehead_temp
+        # outside_temp = self.outside_temp
 
-    @classmethod
-    def _distance_comp(cls, temp, distance=20):
+    @staticmethod
+    def distance_compensation(temp, distance=20):
         """距离补偿"""
         temp = 0.5179 * distance + 0.9436 * distance - 0.0139 * distance * temp + 2.1898
         return temp
 
-
-if __name__ == '__main__':
-    obj = TempComp(wrist_temp=30, outside_temp=24)
-    print(obj.forehead_temp)
-    obj2 = TempComp(forehead_temp=obj.forehead_temp, outside_temp=24)
-    print(obj2.wrist_temp)
+# if __name__ == '__main__':
+#     obj = TempComp(wrist_temp=30, outside_temp=24)
+#     print(obj.forehead_temp)
+#     obj2 = TempComp(forehead_temp=obj.forehead_temp, outside_temp=24)
+#     print(obj2.wrist_temp)
